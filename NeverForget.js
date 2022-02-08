634b4bef3f07af71e4c54b5448cb2b7c6442e67f
chery-pynya <nikita.cherepnya@gmail.com>
2022-02-08 16:12:34 +0300

    исправлен регепс, добавлена возможность сохраниения напоминания

--- NeverForget.js
+++ NeverForget.js
@@ -1,42 +1,125 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const messages_1 = __importDefault(require("./messages"));
 const regexp_1 = require("./regexp");
-const settings = {
-    "reply_markup": JSON.stringify({
+const initialReminderState = {
+    status: false,
+    dateSend: false,
+    date: '',
+    timeSend: false,
+    time: '',
+    textSend: false,
+    text: ''
+};
+const menu = {
+    "reply_markup": {
         inline_keyboard: [
-            [{ text: 'Настроить', callback_data: '/settings' }]
+            [
+                { text: 'Новое напоминание', callback_data: '/newReminder' },
+                { text: 'Настройки', callback_data: '/settings' },
+                { text: 'Показать все напоминания', callback_data: '/showList' },
+                { text: 'Удалить напоминание', callback_data: '/deleteMessage' },
+            ]
         ]
-    })
+    }
 };
 class NeverForget {
     constructor(bot) {
         this.bot = bot;
         this.reminders = [];
         this.user = 0;
+        this.reminderState = initialReminderState;
     }
+    //запуск бота и всех его методов
     init() {
         this.startBot();
-        this.setReminder();
+        this.listenForCallback();
+        this.listenForMessage();
     }
+    //обработка команды /start, сохранияем id юзера и показываем ему меню
     startBot() {
         this.bot.onText(/\/start/, (msg) => {
+            console.log(msg);
             if (msg.from !== undefined) {
                 this.user = msg.from.id;
             }
-            this.bot.sendMessage(this.user, messages_1.default.greeting, settings);
-        });
-        this.bot.on('callback_query', (msg) => {
-            console.log(msg);
+            return this.bot.sendMessage(this.user, messages_1.default.greeting, menu);
         });
     }
-    setReminder() {
-        this.bot.on('message', (msg) => {
-            this.bot.sendMessage(this.user, 'выбери дату');
+    //слушаем callback_query
+    listenForCallback() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.bot.on('callback_query', (msg) => {
+                let data;
+                if (msg.data !== undefined) {
+                    data = msg.data;
+                    if (data === '/newReminder') {
+                        this.reminderState.status = true;
+                        this.reminderState.dateSend = true;
+                        return this.bot.sendMessage(this.user, 'Введите число в формате ДД.ММ.ГГГГ.');
+                    }
+                }
+                return this.bot.sendMessage(this.user, 'Неизыестная команда, Попробуйте выбрать одну их этих команд:', menu);
+            });
         });
+    }
+    //слушаем сообщения пользователя
+    listenForMessage() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.bot.on('message', (msg) => {
+                const { status } = this.reminderState;
+                if (status) {
+                    const { dateSend, timeSend, textSend } = this.reminderState;
+                    if (msg.text !== undefined) {
+                        const text = msg.text.trim();
+                        //проверяем дату и сохранияем ее
+                        if (dateSend && regexp_1.regDate.test(text)) {
+                            //нужно добавить проверку даты, чтобы она была в будущем
+                            this.reminderState.date = text;
+                            this.reminderState.timeSend = true;
+                            return this.bot.sendMessage(this.user, 'Отлично! А теперь введите время в формате ЧЧ:ММ.');
+                        }
+                        //сохранияем время
+                        if (dateSend && timeSend && regexp_1.regTime.test(text)) {
+                            this.reminderState.time = text;
+                            this.reminderState.textSend = true;
+                            return this.bot.sendMessage(this.user, 'Круто! А теперь введите текст напоминания');
+                        }
+                        //сохранияем текс, сохраняем напоминание и очмщаес стейт
+                        if (dateSend && timeSend && textSend) {
+                            this.reminderState.text = text;
+                            const { date, time } = this.reminderState;
+                            this.saveReminder(date, time, text);
+                            this.reminderState = initialReminderState;
+                            return this.bot.sendMessage(this.user, 'Поздравляю! Напоминание сохранено', menu);
+                        }
+                        return this.bot.sendMessage(this.user, 'Не совсем Вас понял, попробуйте еще раз!');
+                    }
+                }
+                return this.bot.sendMessage(this.user, 'бла, бла, бла! тупые слова!');
+            });
+        });
+    }
+    //сохранияем уведомление
+    saveReminder(date, time, reminder) {
+        const item = {
+            text: reminder,
+            id: this.user,
+            date: this.getDateFromSting(date, time),
+        };
+        this.reminders.push(item);
     }
     //проверяем сообщение на соотвествие шаблону
     checkForReminder(str) {
@@ -48,14 +131,14 @@ class NeverForget {
         }
         return false;
     }
-    //парсим строку с датой и создаем объект даты
-    getStringForDate(d, t) {
+    //парсим строку с датой и создаем строку из объекта даты
+    getDateFromSting(d, t) {
         const day = +(d.slice(0, 2));
         const month = +(d.slice(3, 5)) - 1;
         const year = +(d.slice(6));
         const hour = +(t.slice(0, 2));
         const mins = +(t.slice(3));
-        return new Date(year, month, day, hour, mins, 0);
+        return (new Date(year, month, day, hour, mins, 0)).toString();
     }
 }
 exports.default = NeverForget;
--- NeverForget.ts
+++ NeverForget.ts
@@ -1,51 +1,135 @@
-import TelegramApi from 'node-telegram-bot-api';
+import TelegramApi, { SendMessageOptions, Message } from 'node-telegram-bot-api';
 import messages from './messages';
 import { regTime, regDate } from './regexp';
 
 interface Reminder {
-    date: Date,
-    reminder: string
+    date: string,
+    text: string,
+    id: number,
 }
 
-const settings = {
-    "reply_markup": JSON.stringify({
-        inline_keyboard: [
-            [{text: 'Настроить', callback_data: '/settings'}]
+interface ReminderState {
+    status: boolean
+    dateSend: boolean
+    date: string
+    timeSend: boolean
+    time: string
+    textSend: boolean
+    text: string
+}
+
+const initialReminderState: ReminderState = {
+    status: false,
+    dateSend: false,
+    date: '',
+    timeSend: false,
+    time: '',
+    textSend: false,
+    text: ''
+}
+
+const menu: SendMessageOptions = {
+    "reply_markup": {
+        inline_keyboard:  [
+            [
+                {text: 'Новое напоминание', callback_data: '/newReminder'},
+                {text: 'Настройки', callback_data: '/settings'},
+                {text: 'Показать все напоминания', callback_data: '/showList'},
+                {text: 'Удалить напоминание', callback_data: '/deleteMessage'},
+            ]
         ]
-    })
+    }
 }
 
 export default class NeverForget {
     public reminders: Array<Reminder>;
     public user: number;
+    public reminderState: ReminderState
     constructor(
         public bot: TelegramApi,
     ) {
         this.reminders = [];
         this.user = 0;
+        this.reminderState = initialReminderState;
     }
 
+    //запуск бота и всех его методов
     init(): void {
         this.startBot();
-        this.setReminder();
+        this.listenForCallback();
+        this.listenForMessage();
     }
 
+    //обработка команды /start, сохранияем id юзера и показываем ему меню
     startBot(): void {
-        this.bot.onText(/\/start/, (msg): void => {
+        this.bot.onText(/\/start/, (msg): Promise<Message> => {
+            console.log(msg)
             if (msg.from !== undefined) {
                 this.user = msg.from.id;
             }
-            this.bot.sendMessage(this.user, messages.greeting, settings as any);
-        });
-        this.bot.on('callback_query', (msg): void => {
-            console.log(msg)
+            return this.bot.sendMessage(this.user, messages.greeting, menu);
         });
     }
 
-    setReminder(): void {
-        this.bot.on('message', (msg): void => {
-            this.bot.sendMessage(this.user, 'выбери дату');
+    //слушаем callback_query
+    async listenForCallback(): Promise<void> {
+        this.bot.on('callback_query', (msg): Promise<Message> => {
+            let data: string;
+            if (msg.data !== undefined) {
+                data = msg.data;
+                if (data ==='/newReminder') {
+                    this.reminderState.status = true;
+                    this.reminderState.dateSend = true;
+                    return this.bot.sendMessage(this.user, 'Введите число в формате ДД.ММ.ГГГГ.');
+                }
+            }
+            return this.bot.sendMessage(this.user, 'Неизыестная команда, Попробуйте выбрать одну их этих команд:', menu);
         });
+    }
+    //слушаем сообщения пользователя
+    async listenForMessage(): Promise<void> {
+        this.bot.on('message', (msg): Promise<Message> => {
+            const { status }: { status: boolean } = this.reminderState; 
+            if (status) {
+                const { dateSend, timeSend, textSend }: { dateSend: boolean, timeSend: boolean, textSend: boolean  } = this.reminderState; 
+                if (msg.text !== undefined) {
+                    const text: string = msg.text.trim();
+                    //проверяем дату и сохранияем ее
+                    if (dateSend && regDate.test(text)) {
+                        //нужно добавить проверку даты, чтобы она была в будущем
+                        this.reminderState.date = text;
+                        this.reminderState.timeSend = true;
+                        return this.bot.sendMessage(this.user, 'Отлично! А теперь введите время в формате ЧЧ:ММ.');
+                    }
+                    //сохранияем время
+                    if (dateSend && timeSend && regTime.test(text)) {
+                        this.reminderState.time = text;
+                        this.reminderState.textSend = true;
+                        return this.bot.sendMessage(this.user, 'Круто! А теперь введите текст напоминания');
+                    }
+                    //сохранияем текс, сохраняем напоминание и очмщаес стейт
+                    if (dateSend && timeSend && textSend) {
+                        this.reminderState.text = text;
+                        const { date, time }: {date: string, time: string } = this.reminderState
+                        this.saveReminder(date, time, text);
+                        this.reminderState = initialReminderState;
+                        return this.bot.sendMessage(this.user, 'Поздравляю! Напоминание сохранено', menu);
+                    }
+                    return this.bot.sendMessage(this.user, 'Не совсем Вас понял, попробуйте еще раз!');
+                }
+            }
+            return this.bot.sendMessage(this.user, 'бла, бла, бла! тупые слова!');
+        });
+    }
+
+    //сохранияем уведомление
+    private saveReminder(date: string, time: string, reminder: string): void {
+        const item: Reminder = {
+            text: reminder,
+            id: this.user,
+            date: this.getDateFromSting(date, time),
+        }
+        this.reminders.push(item);
     }
 
     //проверяем сообщение на соотвествие шаблону
@@ -58,13 +142,13 @@ export default class NeverForget {
         }
         return false;
     }
-    //парсим строку с датой и создаем объект даты
-    getStringForDate(d: string, t: string): Date {
+    //парсим строку с датой и создаем строку из объекта даты
+    public getDateFromSting(d: string, t: string): string {
         const day: number = +(d.slice(0, 2));
         const month: number = +(d.slice(3, 5)) - 1;
         const year: number = +(d.slice(6));
         const hour: number = +(t.slice(0, 2));
         const mins: number = +(t.slice(3));
-        return new Date(year, month, day, hour, mins, 0);
+        return (new Date(year, month, day, hour, mins, 0)).toString();
     }
 };
